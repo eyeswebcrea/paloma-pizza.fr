bb74cd40bcc88b854801f5f3dd0938eb8fb2e4b7
root <root@ubuntu.(none)>
2013-04-13 12:32:33 +0200

    first commit

---
+++ src/Site/HomeBundle/Controller/DefaultController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Site\HomeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+    
+    public function indexAction()
+    {
+        return $this->render('SiteHomeBundle:Default:index.html.twig');
+    }
+	
+	public function viewPageContactAction()
+	{
+		return $this->render("SiteHomeBundle:Default:contact.html.twig");
+	}
+	
+	public function viewPageLaCarteAction()
+	{
+		return $this->render("SiteHomeBundle:Default:lacarte.html.twig");
+	}
+	
+	public function viewPageLaBricopubAction()
+	{
+		return $this->render("SiteHomeBundle:Default:labricopub.html.twig");
+	}
+	
+}
